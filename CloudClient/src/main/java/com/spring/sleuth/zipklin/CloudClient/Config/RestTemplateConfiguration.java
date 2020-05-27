4ff828786041258bc354b5e15ca118443ac56bf4
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-27 13:23:17 +0530

    Rest Template Configuration

--- CloudClient/src/main/java/com/spring/sleuth/zipklin/CloudClient/Config/RestTemplateConfiguration.java
+++ CloudClient/src/main/java/com/spring/sleuth/zipklin/CloudClient/Config/RestTemplateConfiguration.java
@@ -1,21 +1,15 @@
 package com.spring.sleuth.zipklin.CloudClient.Config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
-@RequestMapping("/rest")
+
+@Configuration
 public class RestTemplateConfiguration {
 
-	private static final Logger log=LoggerFactory.getLogger(RestTemplateConfiguration.class);
-	
-	@GetMapping(value = "/server")
-    public String hello() {
-		log.info("Reached Server");
-        return "Hi! This is Sleuth-Zipkin Server";
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
-	
 }
