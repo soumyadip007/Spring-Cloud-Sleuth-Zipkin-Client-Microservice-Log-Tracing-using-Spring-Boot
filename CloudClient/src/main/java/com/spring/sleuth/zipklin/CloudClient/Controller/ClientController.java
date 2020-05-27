ce551b6fecf4a8718aad7356500fd2bd029bad79
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-27 13:25:08 +0530

    restTemplate

--- CloudClient/src/main/java/com/spring/sleuth/zipklin/CloudClient/Controller/ClientController.java
+++ CloudClient/src/main/java/com/spring/sleuth/zipklin/CloudClient/Controller/ClientController.java
@@ -2,9 +2,11 @@ package com.spring.sleuth.zipklin.CloudClient.Controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/rest")
@@ -12,7 +14,10 @@ public class ClientController {
 
 	private static final Logger log=LoggerFactory.getLogger(ClientController.class);
 	
-	@GetMapping(value = "/server")
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping(value = "/clinet")
     public String hello() {
 		log.info("Reached Server");
         return "Hi! This is Sleuth-Zipkin Server";
