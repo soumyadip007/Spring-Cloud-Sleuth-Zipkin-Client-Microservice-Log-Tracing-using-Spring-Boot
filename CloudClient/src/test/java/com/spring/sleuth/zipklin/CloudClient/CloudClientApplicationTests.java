f3476b47c23033b826648a7f84c2455962219828
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-27 12:59:03 +0530

    Sleuth Zipkin Client Test

---
+++ CloudClient/src/test/java/com/spring/sleuth/zipklin/CloudClient/CloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sleuth.zipklin.CloudClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
